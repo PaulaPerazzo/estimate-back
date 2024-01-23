5636321f261480638607c5d43f021a6da6200131
luis_mota <lfam@cin.ufpe.br>
2024-01-23 16:59:18 -0300

    Atualizando Usuario/conexão banco

--- app/main.py
+++ app/main.py
@@ -10,14 +10,9 @@ model.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:8000",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
