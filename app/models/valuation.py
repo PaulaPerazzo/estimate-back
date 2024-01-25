62b5ba7bed7fd66d6a4fc456e38cdb063f05b0fe
paulaperazzo <mpps@cin.ufpe.br>
2024-01-25 16:45:48 -0300

    valuation table

---
+++ app/models/valuation.py
@@ -0,0 +1,19 @@
+# models.py
+from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String
+from database.config import Base
+
+
+### TABLE valuation (input model (ValuationBase) and table columns (Valuation))
+class ValuationBase(BaseModel):
+    valuation: str
+    user_id: str
+    task_id: str
+
+class Valuation(Base):
+    __tablename__ = "valuations"
+
+    id = Column(Integer, primary_key=True, index=True)
+    valuation = Column(String(500), nullable=False)
+    user_id = Column(Integer, nullable=True)
+    task_id = Column(Integer, nullable=True)
